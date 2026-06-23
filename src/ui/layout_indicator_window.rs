43fab75a17fe1661297f722d0a72eb4b920f8b72
kissetfall <j@ergohaven.xyz>
2026-06-23 15:57:18 +0300

    Return focus immediately after layer OSD

--- src/ui/layout_indicator_window.rs
+++ src/ui/layout_indicator_window.rs
@@ -386,7 +386,6 @@ impl EntropyApp {
             .with_decorations(false)
             .with_taskbar(false)
             .with_transparent(true)
-            .with_active(false)
             .with_mouse_passthrough(true)
             .with_window_type(egui::X11WindowType::Utility)
             .with_window_level(egui::WindowLevel::AlwaysOnTop);
@@ -439,6 +438,7 @@ impl EntropyApp {
                     }
                 });
         });
+        ctx.send_viewport_cmd(egui::ViewportCommand::Focus);
     }
 
     pub(super) fn draw_sticky_layout_window(&mut self, ctx: &egui::Context) {
