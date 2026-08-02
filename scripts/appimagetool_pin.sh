4988ac7e7a3e0d7c07a585161b22411cda688bf7
Igor Arkhipov <igor.arkhipov@joinhandshake.com>
2026-08-01 18:34:32 -0700

    Pin AppImageTool to immutable release

--- scripts/appimagetool_pin.sh
+++ scripts/appimagetool_pin.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# These constants are consumed by scripts that source this file.
+# These constants are consumed by scripts that source this file. Update them
+# with `bash scripts/update_appimagetool_pin.sh <immutable-release-tag>`.
 # shellcheck disable=SC2034
-readonly APPIMAGETOOL_PINNED_URL="https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-readonly APPIMAGETOOL_PINNED_SHA256="b90f4a8b18967545fda78a445b27680a1642f1ef9488ced28b65398f2be7add2"
+readonly APPIMAGETOOL_PINNED_RELEASE="12"
+readonly APPIMAGETOOL_PINNED_URL="https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
+readonly APPIMAGETOOL_PINNED_SHA256="d918b4df547b388ef253f3c9e7f6529ca81a885395c31f619d9aaf7030499a13"
